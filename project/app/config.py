049a50e968055ebee154c4a7fc5640b7b7e624d7
gianPDomiziani <gianpio@csw01599.consoft.it>
2021-09-08 13:39:44 +0200

    postgres service added. Tortoise ORM summary text started.

--- project/app/config.py
+++ project/app/config.py
@@ -2,13 +2,14 @@ import logging
 import os
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 log = logging.getLogger("uvicorn")
 
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.getenv("DATABSE_URL")
 
 @lru_cache
 def get_settings() -> BaseSettings:
