9f6861df48979afcfad193b7dd078eb7a041e07a
gianPDomiziani <gianpio@csw01599.consoft.it>
2021-09-14 13:31:38 +0200

    test summary added.

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -4,6 +4,13 @@ from app.config import get_settings, Settings
 
 router = APIRouter()
 
+@router.get('/')
+async def home(setting: Settings = Depends(get_settings)):
+    return {
+        'Home': 'text summaries home'
+    }
+
+
 @router.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
